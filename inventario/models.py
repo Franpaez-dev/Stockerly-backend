e2f2734325aa620425b74de8c90741f035b5633f
Fran <fullflancito@gmail.com>
2026-03-16 00:12:39 -0300

    Arquitectura multi-tenant terminada: soporte para Supermercado y Ropa

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Comercio(models.Model):
+    TIPO_CHOICES = [
+        ('RETAIL', 'Supermercado/Kiosco'),
+        ('TEXTIL', 'Indumentaria/Ropa'),
+    ]
+    nombre = models.CharField(max_length=150)
+    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
+    direccion = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.get_tipo_display()})"
+
+class Categoria(models.Model):
+    comercio = models.ForeignKey(Comercio, on_delete=models.CASCADE, related_name='categorias', null=True, blank=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.comercio.nombre}"
+
+class Producto(models.Model):
+    comercio = models.ForeignKey(Comercio, on_delete=models.CASCADE, related_name='productos', null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+    nombre = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    
+    # Campos específicos (pueden quedar vacíos según el rubro)
+    codigo_barras = models.CharField(max_length=50, blank=True, null=True) # Más para el súper
+    talle = models.CharField(max_length=10, blank=True, null=True)         # Para la de ropa
+    color = models.CharField(max_length=50, blank=True, null=True)         # Para la de ropa
+
+    def __str__(self):
+        return f"{self.nombre} ({self.comercio.nombre})"
